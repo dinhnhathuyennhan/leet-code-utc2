54cefcb9198886043d6d25400e5701b3ccec8a41
QuocDai <quocdai101105@gmail.com>
2026-09-17 14:51:00 +0700

    bugfix - repair endpoint /teachers and /students

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,7 @@ from app.routers import auth, user
 
 app = FastAPI(title="Leet Code UTC2")
 
-app.include_router(user.router, prefix="/users", tags=["users"])
+app.include_router(user.router, tags=["users"])
 app.include_router(auth.router)
 
 register_exception_handlers(app)
